8b159e3139b2931c0679b345c7b45d83817e9a15
dennis903 <hyeongjin3@hanmail.net>
2021-07-18 00:49:12 +0900

    todo : bureaucrat.cpp

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+# include <iostream>
+# include <string>
+# include <exception>
+
+class   Bureaucrat
+{
+    private:
+        const std::string   name;
+        int                 grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string &_name, int _grade);
+        Bureaucrat(const Bureaucrat &_Bureaucrat);
+        ~Bureaucrat();
+        Bureaucrat &operator = (const Bureaucrat &_Bureaucrat);
+        const std::string getName() const;
+        int getGrade() const;
+        void    IncrementGrade();
+        void    DecrementGrade();
+        class   GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const;
+        }
+};
+#endif
